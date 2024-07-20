b351f60f5867a0983ccb2e37faa8d9b11000983a
trungnhan <trungnhanforwork@gmail.com>
2024-07-20 21:11:18 +0700

    feat(): init project, configuring nlog service

---
+++ FStore/Program.cs
@@ -0,0 +1,53 @@
+using FStore.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+using NLog.Extensions.Logging;
+using NLog.Web;
+var builder = WebApplication.CreateBuilder(args);
+LogManager.Setup().LoadConfigurationFromSection(builder.Configuration);
+
+var logger = LogManager.GetCurrentClassLogger();
+try
+{
+    logger.Debug("init main");
+    // Add services to the container.
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.ConfigureCors();
+    builder.Services.ConfigureIISIntegration();
+    builder.Services.ConfigureLoggerService();
+    builder.Services.AddControllers();
+    
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    else
+    {
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseForwardedHeaders(new ForwardedHeadersOptions
+    {
+        ForwardedHeaders = ForwardedHeaders.All
+    });
+    app.UseCors("CorsPolicy");
+    app.MapControllers();
+    app.Run();
+}
+catch (Exception exception)
+{
+    // NLog: catch setup errors
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    LogManager.Shutdown();
+}
